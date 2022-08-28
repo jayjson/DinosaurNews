6461a05e75fa68d581eb0afaea694d0acf0fd5d0
Jay <10294167+jayjson@users.noreply.github.com>
2022-08-28 14:30:34 -0400

    Updated property types

--- app/src/main/java/com/jayjson/dinosaurnews/Article.kt
+++ app/src/main/java/com/jayjson/dinosaurnews/Article.kt
@@ -1,10 +1,10 @@
 package com.jayjson.dinosaurnews
 
 data class Article(val source: Source,
-                   val author: String,
+                   val author: String? = null,
                    val title: String,
                    val description: String,
                    val url: String,
-                   val urlToImage: String,
+                   val urlToImage: String? = null,
                    val publishedAt: String,
                    val content: String)
--- app/src/main/java/com/jayjson/dinosaurnews/Source.kt
+++ app/src/main/java/com/jayjson/dinosaurnews/Source.kt
@@ -1,9 +1,9 @@
 package com.jayjson.dinosaurnews
 
-data class Source(val id: String,
+data class Source(val id: String? = null,
                   val name: String,
-                  val description: String,
-                  val url: String,
-                  val category: String,
-                  val language: String,
-                  val country: String)
+                  val description: String? = null,
+                  val url: String? = null,
+                  val category: String? = null,
+                  val language: String? = null,
+                  val country: String? = null)
