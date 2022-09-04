d06b4c90443494f3d109d22e12f3595e00056f7c
Jay <10294167+jayjson@users.noreply.github.com>
2022-09-04 12:38:29 -0400

    added enum classes
    i.e., SourceCategory, Language, Country

--- app/src/main/java/com/jayjson/dinosaurnews/Source.kt
+++ app/src/main/java/com/jayjson/dinosaurnews/Source.kt
@@ -7,3 +7,21 @@ data class Source(val id: String? = null,
                   val category: String? = null,
                   val language: String? = null,
                   val country: String? = null)
+
+enum class SourceCategory {
+    BUSINESS,
+    ENTERTAINMENT,
+    GENERAL,
+    HEALTH,
+    SCIENCE,
+    SPORTS,
+    TECHNOLOGY
+}
+
+enum class Language {
+    AR, DE, EN, ES, FR, HE, IT, NL, NO, PT, RU, SV, UD, ZH
+}
+
+enum class Country {
+    AE, AR, AT, AU, BE, BG, BR, CA, CH, CN, CO, CU, CZ, DE, EG, FR, GB, GR, HK, HU, ID, IE, IL, IN, IT, JP, KR, LT, LV, MA, MX, MY, NG, NL, NO, NZ, PH, PL, PT, RO, RS, RU, SA, SE, SG, SI, SK, TH, TR, TW, UA, US, VE, ZA
+}
