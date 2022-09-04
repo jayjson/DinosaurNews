feec43f3c679e3d5b497a3657bd7030cb93e5d95
Jay <10294167+jayjson@users.noreply.github.com>
2022-09-04 13:02:13 -0400

    created files for enum classes

---
+++ app/src/main/java/com/jayjson/dinosaurnews/models/Language.kt
@@ -0,0 +1,5 @@
+package com.jayjson.dinosaurnews.models
+
+enum class Language {
+    AR, DE, EN, ES, FR, HE, IT, NL, NO, PT, RU, SV, UD, ZH
+}
---
+++ app/src/main/java/com/jayjson/dinosaurnews/models/SourceCategory.kt
@@ -0,0 +1,11 @@
+package com.jayjson.dinosaurnews.models
+
+enum class SourceCategory {
+    BUSINESS,
+    ENTERTAINMENT,
+    GENERAL,
+    HEALTH,
+    SCIENCE,
+    SPORTS,
+    TECHNOLOGY
+}
