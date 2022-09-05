d2a70eed488c389d98588f19012f0076828895c8
Jay <10294167+jayjson@users.noreply.github.com>
2022-09-05 12:46:03 -0400

    Updated ArticleView

--- app/src/main/java/com/jayjson/dinosaurnews/ArticleView.kt
+++ app/src/main/java/com/jayjson/dinosaurnews/ArticleView.kt
@@ -2,6 +2,20 @@ package com.jayjson.dinosaurnews
 
 import android.content.Context
 import android.util.AttributeSet
-import android.view.View
+import android.view.LayoutInflater
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.jayjson.dinosaurnews.databinding.ArticleViewBinding
+import com.jayjson.dinosaurnews.models.Article
 
-class ArticleView(context: Context, attrs: AttributeSet): View(context, attrs)
+class ArticleView(
+    context: Context,
+    attrs: AttributeSet? = null
+): ConstraintLayout(context, attrs) {
+    private val binding = ArticleViewBinding.inflate(LayoutInflater.from(context), this)
+
+    fun setData(article: Article) {
+        binding.titleTextView.text = article.title
+        val authorName = if (article.author != null) article.author else "unknown author"
+        binding.authorTextView.text =  "by ${authorName}"
+    }
+}
