f5d3cc22632cef8153ea081236751a9d26d4a7f0
Jay <10294167+jayjson@users.noreply.github.com>
2022-09-18 23:05:21 -0400

    Implemented the App class

---
+++ app/src/main/java/com/jayjson/dinosaurnews/App.kt
@@ -0,0 +1,37 @@
+package com.jayjson.dinosaurnews
+
+import android.app.Application
+import android.content.Context
+import com.jayjson.dinosaurnews.networking.RemoteApi
+import com.jayjson.dinosaurnews.networking.buildApiService
+
+private const val KEY_PREFERENCES = "taskie_preferences"
+private const val KEY_TOKEN = "token"
+
+class App : Application() {
+
+    companion object {
+        private lateinit var instance: App
+
+        private val preferences by lazy {
+            instance.getSharedPreferences(KEY_PREFERENCES, Context.MODE_PRIVATE)
+        }
+
+        fun saveToken(token: String) {
+            preferences.edit()
+                .putString(KEY_TOKEN, token)
+                .apply()
+        }
+
+        fun getToken() = preferences.getString(KEY_TOKEN, "") ?: ""
+
+        private val apiService by lazy { buildApiService() }
+
+        val remoteApi by lazy { RemoteApi(apiService) }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
+}
