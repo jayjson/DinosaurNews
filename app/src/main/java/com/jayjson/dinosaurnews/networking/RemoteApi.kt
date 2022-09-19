ed36a8fb6d20abd859db6feb8bf9d8c0318e4aa6
Jay <10294167+jayjson@users.noreply.github.com>
2022-09-18 23:05:52 -0400

    Implemented the getTopHeadlines method

--- app/src/main/java/com/jayjson/dinosaurnews/networking/RemoteApi.kt
+++ app/src/main/java/com/jayjson/dinosaurnews/networking/RemoteApi.kt
@@ -1,6 +1,31 @@
 package com.jayjson.dinosaurnews.networking
 
+import com.jayjson.dinosaurnews.App
+import com.jayjson.dinosaurnews.models.Article
+import com.jayjson.dinosaurnews.models.response.GetTopHeadlinesResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
 const val BASE_URL = "https://newsapi.org"
 
 class RemoteApi(private val apiService: RemoteApiService) {
+
+    fun getTopHeadlines(onTasksReceived: (List<Article>, Throwable?) -> Unit) {
+        apiService.getTopHeadlines(App.getToken()).enqueue(object : Callback<GetTopHeadlinesResponse> {
+            override fun onFailure(call: Call<GetTopHeadlinesResponse>, error: Throwable) {
+                onTasksReceived(emptyList(), error)
+            }
+
+            override fun onResponse(call: Call<GetTopHeadlinesResponse>, response: Response<GetTopHeadlinesResponse>) {
+                val data = response.body()
+
+                if (data != null && data.articles.isNotEmpty()) {
+                    onTasksReceived(data.articles, null)
+                } else {
+                    onTasksReceived(emptyList(), NullPointerException("No data available!"))
+                }
+            }
+        })
+    }
 }
