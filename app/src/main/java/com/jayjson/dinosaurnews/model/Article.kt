de16175a271f0a7cfeeaec4a779032d976953a06
Jay <10294167+jayjson@users.noreply.github.com>
2022-10-09 10:44:39 -0400

    Converted Article and Source into database entities

--- app/src/main/java/com/jayjson/dinosaurnews/model/Article.kt
+++ app/src/main/java/com/jayjson/dinosaurnews/model/Article.kt
@@ -1,13 +1,21 @@
 package com.jayjson.dinosaurnews.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.jayjson.dinosaurnews.Source
 import java.io.Serializable
+import java.util.*
 
-data class Article(val source: Source,
-                   val author: String? = null,
-                   val title: String,
-                   val description: String,
-                   val url: String,
-                   val urlToImage: String? = null,
-                   val publishedAt: String,
-                   val content: String) : Serializable
+@Entity(tableName = "articles")
+data class Article(
+    @PrimaryKey
+    val id: String = UUID.randomUUID().toString(),
+    val source: Source,
+    val author: String? = null,
+    val title: String,
+    val description: String,
+    val url: String,
+    val urlToImage: String? = null,
+    val publishedAt: String,
+    val content: String
+) : Serializable
--- app/src/main/java/com/jayjson/dinosaurnews/model/Source.kt
+++ app/src/main/java/com/jayjson/dinosaurnews/model/Source.kt
@@ -1,14 +1,20 @@
 package com.jayjson.dinosaurnews
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.jayjson.dinosaurnews.model.Country
 import com.jayjson.dinosaurnews.model.Language
 import com.jayjson.dinosaurnews.model.SourceCategory
 import java.io.Serializable
 
-data class Source(val id: String? = null,
-                  val name: String,
-                  val description: String? = null,
-                  val url: String? = null,
-                  val category: SourceCategory? = null,
-                  val language: Language? = null,
-                  val country: Country? = null) : Serializable
+@Entity(tableName = "sources")
+data class Source(
+    @PrimaryKey
+    val id: String? = null,
+    val name: String,
+    val description: String? = null,
+    val url: String? = null,
+    val category: SourceCategory? = null,
+    val language: Language? = null,
+    val country: Country? = null
+) : Serializable
